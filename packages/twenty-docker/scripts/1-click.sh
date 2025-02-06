83816e1ea47df1d3fdb81a5c3e0a2338be8d5163
salahzsh <me@salah.sh>
2025-02-06 13:27:31 +0100

    Update instlal script

--- packages/twenty-docker/scripts/1-click.sh
+++ packages/twenty-docker/scripts/1-click.sh
@@ -1,12 +1,12 @@
-pull_version=${VERSION:-$(curl -s https://api.github.com/repos/salahzsh/twenty/releases | grep '"name":' | head -n 1 | cut -d '"' -f 4)}
-pull_version=v0.0.0-test
+#pull_version=${VERSION:-$(curl -s https://api.github.com/repos/salahzsh/twenty/releases | grep '"name":' | head -n 1 | cut -d '"' -f 4)}
+#pull_version=v0.0.0-test
 pull_version=latest
 
 if [[ -z "$pull_version" ]]; then
   echo "Error: Unable to fetch the latest version tag. Please check your network connection or the GitHub API response."
   exit 1
 fi
-pull_branch=${BRANCH:-$pull_version}
+#pull_branch=${BRANCH:-$pull_version}
 pull_branch=main
 
 curl -sL "https://raw.githubusercontent.com/salahzsh/twenty/$pull_branch/packages/twenty-docker/scripts/install.sh" -o twenty_install.sh
